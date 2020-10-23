fae563d017cfa38077725420daa1bf9b21eddd5a
Ryan <rcfieldhouse@yahoo.com>
2020-10-23 15:05:31 -0400

    creates first set of cards

--- GDW Project GUI/Gameplay.cpp
+++ GDW Project GUI/Gameplay.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 #include <math.h>
+#include <time.h>
+#define cardNumber 4
+#define playerCount 2
+
 using namespace std;
 
-int gameplay() {
+//data types 0=suit, 1=colour,2=point value
+int main() {
+	static int hand[playerCount][cardNumber][3]{ };
+	for (int i = 0; i <= playerCount; i++) {
+		for (int j = 0; j <= cardNumber; j++) {
+			for (int k = 0; k <= 2; k++) {
+				switch (k) {
+				case(0):hand[i][j][k] = (rand() % 6) + 1;
+				case(1):hand[i][j][k] = (rand() % 8) + 1;
+				case(2):hand[i][j][k] = (rand() % 8) + 1;
 
+				}
+			}
+		}
+	}
+	cout << hand[1][3][2] << endl;
+	cout << hand[0][1][0] << endl;
+	cout << hand[1][2][0] << endl;
+	return (0);
 }
+
