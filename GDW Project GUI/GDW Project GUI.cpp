362a1eb90cc139e9df14cb736a5b49912beb0a85
Dante Arruda <dante.arruda416@gmail.com>
2020-10-23 16:00:38 -0400

    Merged cpp files

--- GDW Project GUI/GDW Project GUI.cpp
+++ GDW Project GUI/GDW Project GUI.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
-#include "Gameplay.h"
+#include <math.h>
+#include <time.h>
+#define cardNumber 4
+#define playerCount 2
+using namespace std;
+//data types 0=suit, 1=colour,2=point value
+static int hand[playerCount][cardNumber][3]{ };
+
+static void createCard(int player, int cardNum) {
+
+	for (int k = 0; k <= 2; k++) {
+		switch (k) {
+		case(0):hand[player][cardNum][k] = (rand() % 6) + 1;
+		case(1):hand[player][cardNum][k] = (rand() % 8) + 1;
+		case(2):hand[player][cardNum][k] = (rand() % 8) + 1;
+		}
+	}
+}
+static int gameStart() {
+
+	//assigns values for all players, cards and data types within cards 
+	for (int i = 0; i <= playerCount; i++) {
+		for (int j = 0; j <= cardNumber; j++) {
+			createCard(i, j);
+		}
+	}
+	return (0);
+}
+
 
 int main()
 {
@@ -50,6 +78,12 @@ int main()
 	std::cout << "2. View the rules" << std::endl;
 	std::cin >> playGame;
 
+	if (playGame == 1) {
+
+		gameStart();
+		
+	}
+
 	// While loop that makes sure the player enter 1 or 2; 
 	while (playGame != 1 && playGame != 2) {
 		std::cout << playGame << " is not a valid option try again ";
@@ -89,4 +123,6 @@ int main()
 	}
 	gameStart();
 }
-// i change 
+
+
+
--- GDW Project GUI/Gameplay.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#define Gamplay_h
-#include <iostream>
-#include <math.h>
-#include <time.h>
-void createCard();
-int gameStart();
